26c52a2694df9489f784eaf1e3c8fb1a42d919e8
lewis <>
2024-01-04 08:55:52 +0800

    feat: add testnet data

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -14,7 +14,27 @@ type CollectionMetadata struct {
 type NftItem struct {
 	gorm.Model
 
-	Name        string `json:"name"`
+	Name        string `json:"name" gorm:"uniqueIndex"`
+	Description string `json:"description"`
+	Image       string `json:"image"`
+	ExternalUrl string `json:"external_url"`
+	Marketplace string `json:"marketplace"`
+}
+
+// TestNet Model
+type CollectionTestnetMetadata struct {
+	gorm.Model
+	Name        string `json:"name" gorm:"uniqueIndex"`
+	Description string `json:"description"`
+	Image       string `json:"image"`
+	CoverImage  string `json:"cover_image"`
+	SocialLinks string `json:"social_links"`
+}
+
+type NftTestnetItem struct {
+	gorm.Model
+
+	Name        string `json:"name" gorm:"uniqueIndex"`
 	Description string `json:"description"`
 	Image       string `json:"image"`
 	ExternalUrl string `json:"external_url"`
--- routers/routers.go
+++ routers/routers.go
@@ -22,5 +22,17 @@ func InitRouter() *gin.Engine {
 	router.POST("/tokentick/create", app.CreateItem)
 	router.GET("/tokentick/get/:name", app.GetItem)
 
+	// TestNet接口
+	// 上传图片的接口
+	router.POST("/testnet/upload", app.UploadHandler)
+
+	// 更新和获取 Collection Metadata 的接口
+	router.POST("/testnet/collection/create", app.CreateTestNetCollectionMetadata)
+	router.GET("/testnet/collection/get/:name", app.GetTestNetCollectionMetadata)
+
+	// 更新和获取 TokenTick Metadata 的接口
+	router.POST("/testnet/tokentick/create", app.CreateTestNetItem)
+	router.GET("/testnet/tokentick/get/:name", app.GetTestNetItem)
+
 	return router
 }
